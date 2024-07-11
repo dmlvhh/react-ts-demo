88525c42e5835f1bdec1f2c0febe83756cb1600d
lv <1967798998@qq.com>
2024-07-11 21:15:32 +0800

    feature:实战列表页

--- src/App.tsx
+++ src/App.tsx
@@ -1,65 +1,40 @@
 import React from 'react'
-import logo from './logo.svg'
 import './App.css'
-import type { MouseEvent } from 'react'
-
+import './App1.css'
 function App() {
-  const fn = (e: MouseEvent<HTMLButtonElement>, name: string) => {
-    e.preventDefault()
-    console.log('clicked...', name)
-  }
-  const style = { color: 'red', backgroundColor: 'white' }
-  const flag = true
-  const Hello = () => {
-    if (flag) return <p>hello1</p>
-    return <p>你好1</p>
-  }
+  // 列表页
   const list = [
-    { username: 'zhangsan', name: '张三' },
-    { username: 'zhangsan1', name: '张三1' },
-    { username: 'zhangsan2', name: '张三2' },
+    { id: 'q1', title: '问卷1', isPublished: false },
+    { id: 'q2', title: '问卷2', isPublished: true },
+    { id: 'q3', title: '问卷3', isPublished: false },
+    { id: 'q4', title: '问卷4', isPublished: true },
   ]
-
+  const edit = (id: string) => {
+    console.log('edit', id)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        {/*<img src={logo} className="App-logo" alt="logo" />*/}
-        {/*<p>{logo}</p>*/}
-        {/*注释*/}
-        {/*不要index作为key*/}
-        <ul>
-          {list.map(user => {
-            const { username, name } = user
-            return <li key={username}>{name}</li>
-          })}
-        </ul>
-        <div>
-          {flag && <p>Hello</p>}
-          {flag ? <p>Hello</p> : <p>你好</p>}
-          <Hello></Hello>
-        </div>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <div>
-          <button
-            onClick={e => {
-              fn(e, '你好')
-            }}
-          >
-            click
-          </button>
-        </div>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-          style={style}
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>问卷列表页</h1>
+      <div>
+        {list.map(question => {
+          const { id, title, isPublished } = question
+          return (
+            <div key={id} className="list-item">
+              <strong>{title}</strong>
+              &nbsp;
+              {isPublished ? <span style={{ color: 'green' }}>已发布</span> : <span>未发布</span>}
+              &nbsp;
+              <button
+                onClick={() => {
+                  edit(id)
+                }}
+              >
+                编辑问卷
+              </button>
+            </div>
+          )
+        })}
+      </div>
     </div>
   )
 }
