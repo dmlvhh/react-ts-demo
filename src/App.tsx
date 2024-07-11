36cb2e54dd4f36987e857c18230ca79f9e4baa7d
lv <1967798998@qq.com>
2024-07-11 21:41:13 +0800

    feature:React组件基础-拆分UI结构

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,11 @@
 import React from 'react'
 import './App.css'
-import './App1.css'
+import List1 from './list1'
 function App() {
-  // 列表页
-  const list = [
-    { id: 'q1', title: '问卷1', isPublished: false },
-    { id: 'q2', title: '问卷2', isPublished: true },
-    { id: 'q3', title: '问卷3', isPublished: false },
-    { id: 'q4', title: '问卷4', isPublished: true },
-  ]
-  const edit = (id: string) => {
-    console.log('edit', id)
-  }
   return (
-    <div>
-      <h1>问卷列表页</h1>
-      <div>
-        {list.map(question => {
-          const { id, title, isPublished } = question
-          return (
-            <div key={id} className="list-item">
-              <strong>{title}</strong>
-              &nbsp;
-              {isPublished ? <span style={{ color: 'green' }}>已发布</span> : <span>未发布</span>}
-              &nbsp;
-              <button
-                onClick={() => {
-                  edit(id)
-                }}
-              >
-                编辑问卷
-              </button>
-            </div>
-          )
-        })}
-      </div>
-    </div>
+    <>
+      <List1 />
+    </>
   )
 }
-
 export default App
