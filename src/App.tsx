1751d6bfb71419d67973af5e33b65c8915307fc2
lv <1967798998@qq.com>
2024-07-11 22:26:06 +0800

    feature:state特点-异步更新，合并后更新

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,10 @@
-import React, { useState } from 'react'
-// import List1 from './list1'
+import React from 'react'
+import StateDemo from './StateDemo'
 function App() {
-  // let count = 0 // 普通的js无法触发组件的更新
-  const [count, setCount] = useState(0) //useState 可以出发组件的更新
-  const add = () => {
-    // count++
-    setCount(count + 1)
-    console.log(count)
-  }
   return (
     <>
       {/*<List1 />*/}
-      <div>
-        <button onClick={add}>add {count}</button>
-      </div>
+      <StateDemo />
     </>
   )
 }
---
+++ src/StateDemo.tsx
@@ -0,0 +1,28 @@
+import React, { FC, useState } from 'react'
+
+const Demo: FC = () => {
+  // let count = 0 // 普通的js无法触发组件的更新
+  const [count, setCount] = useState(0) //useState 可以出发组件的更新
+  const [name, setName] = useState('张三') //useState 可以出发组件的更新
+  const add = () => {
+    // count++
+    // setCount(count + 1) // 合并后更新
+    // setCount(count + 1)
+    // 不会被合并后更新
+    setCount(count => count + 1) // 异步更新无法拿到最新的值
+    setCount(count => count + 1) // 异步更新无法拿到最新的值
+    setCount(count => count + 1)
+    console.log('cur count', count)
+    // setName('x')
+    // console.log(name) // 如果说一个变量 不用于jsx 中显示，那就不要用 setState来管理它，用useRef
+  }
+  return (
+    <>
+      {/*<List1 />*/}
+      <div>
+        <button onClick={add}>add {count}</button>
+      </div>
+    </>
+  )
+}
+export default Demo
