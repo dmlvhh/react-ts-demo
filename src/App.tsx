ad2ce02c03d8db2653282fd29d8ecede82f9ac15
lv <1967798998@qq.com>
2024-07-15 22:27:16 +0800

    feature:第三方Hooks-ahooks

--- src/App.tsx
+++ src/App.tsx
@@ -8,17 +8,23 @@ import React from 'react'
 // import UseCallbackDemo from './UseCallbackDemo'
 // import { useTitle } from './hooks/useTitle'
 // import { useMouse } from './hooks/useMouse'
-import { useGetInfo } from './hooks/useGetInfo'
-
+// import { useGetInfo } from './hooks/useGetInfo'
+import { useTitle } from 'ahooks'
+import { useMouse } from 'ahooks'
 function App() {
   // useTitle('App page 11') //自定义hook
   // const { x, y } = useMouse()
-  const { loading, info } = useGetInfo()
+  // const { loading, info } = useGetInfo()
+  useTitle('App page3')
+  const mouse = useMouse()
   return (
     <>
-      {/*<p>/!*App page {x} {y}*!/</p>*/}
+      {/*<p>App page {x} {y}</p>*/}
+      <p>
+        App page {mouse.pageX} {mouse.pageY}
+      </p>
       <p>App page</p>
-      <p>{loading ? '加载中...' : info}</p>
+      {/*<p>{loading ? '加载中...' : info}</p>*/}
       {/*<List1 />*/}
       {/*<StateDemo1 />*/}
       {/*<StateDemo2 />*/}
