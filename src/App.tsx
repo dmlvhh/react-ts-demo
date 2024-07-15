5c25c6113255b4ce89a06f5e7734783c5922526e
lv <1967798998@qq.com>
2024-07-15 21:39:15 +0800

    feature:自定义Hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import React from 'react'
+import { useTitle } from './hooks/useTitle'
 // import StateDemo1 from './StateDemo1'
 // import StateDemo2 from './StateDemo2'
 // import List2 from './List2'
 // import ImmerDemo from './ImmerDemo'
 // import UseRefDemo from './useRefDemo'
 // import UseMemoDemo from './UseMemoDemo'
-import UseCallbackDemo from './UseCallbackDemo'
+// import UseCallbackDemo from './UseCallbackDemo'
+
 function App() {
+  useTitle('App page 11') //自定义hook
   return (
     <>
+      <p>App page</p>
       {/*<List1 />*/}
       {/*<StateDemo1 />*/}
       {/*<StateDemo2 />*/}
@@ -16,7 +20,7 @@ function App() {
       {/*<ImmerDemo />*/}
       {/*<UseRefDemo />*/}
       {/*<UseMemoDemo />*/}
-      <UseCallbackDemo />
+      {/*<UseCallbackDemo />*/}
     </>
   )
 }
